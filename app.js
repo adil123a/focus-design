220b45b456578b9d9d073bb7a40ece7fe3c1fbad
adil123a <79239820+adil123a@users.noreply.github.com>
2022-07-21 13:28:26 +0600

    Add files via upload
    update website

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 
 //Animate on Scroll Library
 AOS.init({
-    offset: 120,
+  offset: 120,
   delay: 0,
-  duration: 2000, 
+  duration: 1000, 
   easing: 'ease',
 });
 
 //Hide the contextmenu
-var isBody=document.getElementById('body')
+var isBody= document.getElementById('body')
 isBody.addEventListener('contextmenu',e=>{
     e.preventDefault()
+})
+
+//Owl carosel
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  dots:false,
+  autoplayTimeout:1000,
+  autoplay:true,
+  autoplayHoverPause:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
 })
