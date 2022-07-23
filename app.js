155eeb213ac3a115bd76992ac4686a2e8b87d014
adil123a <79239820+adil123a@users.noreply.github.com>
2022-07-23 12:16:54 +0600

    Add files via upload
    update of the website

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ isBody.addEventListener('contextmenu',e=>{
 isBody.addEventListener('selectstart',e=>{
   e.preventDefault()
 })
+
+// custom Cursor
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
